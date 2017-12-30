92e54fbce9cf9ef2410b8753afa9c55ff7a6785f
Eduard - Gabriel <eduard_poe2000@yahoo.com>
2017-12-30 18:26:50 +0200

    Newest Update
    -Implemented TimeUpdate function
    -Implemented FCFS schedule-type
    -Implemented Averages Function
    -Solved conflicts of data

--- SchedSim/include/Scheduler.h
+++ SchedSim/include/Scheduler.h
@@ -10,6 +10,8 @@ public:
     void setSchedulerType(int scht);       // setam scheduler type  1 = FCFS 2 = SJF 3 = RR
     void addProcess(Process tmp);          // adaugam procesul in vectorul de prokese
     void schedule();
+    void calculateAverages();
+    void advanceTime();
 
 protected:
 
@@ -19,20 +21,20 @@ private:
     static bool initialised;
     static Scheduler* sched_instance;
 
-
-    int current_time             // cat timp a trecut de cand am inceput procese
-    int scheduler_type;          // am scris in main care ce si cum :)
-    int cpu_utilisation;         // cat % din CPU e utilizat / cam o sa fie 0 sau 100%
-    int thoroughput;             // numarul de procese terminate in unitatea de timp
-    int turnaround_time_average; // cat timp mi a luat sa termin fiecare proces in medie
-    int waiting_time_average;    // cat timp au asteptat procesele in medie
-    int response_time_average;   // cat timp a durat ca procesul sa fie bagat in seama in medie
+    int current_active_processes;     // numarul de procese active la momentul prezent
+    int current_time = 0;             // cat timp a trecut de cand am inceput procese
+    int scheduler_type;               // am scris in main care ce si cum :)
+    int cpu_utilisation;              // cat % din CPU e utilizat / cam o sa fie 0 sau 100%
+    int thoroughput;                  // numarul de procese terminate in unitatea de timp
+    int turnaround_time_average;      // cat timp mi a luat sa termin fiecare proces in medie
+    int waiting_time_average;         // cat timp au asteptat procesele in medie
+    int response_time_average;        // cat timp a durat ca procesul sa fie bagat in seama in medie
 
     void FCFS();
 
     vector<Process> v;
 
-
+    Process current_process;
 
 };
 
--- SchedSim/src/Scheduler.cpp
+++ SchedSim/src/Scheduler.cpp
@@ -1,11 +1,19 @@
 #include "Scheduler.h"
-
+#include<iostream>
 Scheduler* Scheduler :: sched_instance = nullptr;
 bool Scheduler :: initialised = false;
 
-Scheduler::Scheduler()
+Scheduler::Scheduler()     //ctor
 {
-    //ctor
+
+    current_active_processes = 0;
+    current_time = 0;
+    cpu_utilisation = 0;
+    thoroughput = 0;
+    turnaround_time_average = 0;
+    waiting_time_average = 0;
+    response_time_average = 0;
+
 }
 
 Scheduler::~Scheduler()
@@ -36,15 +44,74 @@ void Scheduler :: addProcess(Process tmp)
 void Scheduler :: FCFS()
 {
 
+    int i  = 0 ;
+    int n  = v.size() ;
+    bool finished;
 
-
-
-
-
+    while( i < n )
+    {
+        current_process = v[i];
+        advanceTime();
+        if(v[i].finish_time !=0)
+            ++i;
+    }
 
 
 }
 
+void Scheduler :: calculateAverages()
+{
+    int i ;
+    int n ;
+
+    n = v.size();
+
+    for(i = 0 ; i < n  ; ++i)
+    {
+        turnaround_time_average +=v[i].finish_time;
+        response_time_average   += v[i].response_time;
+        waiting_time_average    += v[i].waiting_time;
+    }
+
+    turnaround_time_average /= n;
+    response_time_average   /= n;
+    waiting_time_average    /= n;
+
+}
+
+
+
+
+void Scheduler ::advanceTime()
+{
+
+    int i;
+
+    ++current_time;
+
+
+    for(i = 0 ; i < v.size(); ++i)
+    {   Process tmp = v[1];
+        if(current_time >= v[i].start_time)
+        {
+            if(v[i]==current_process)
+            {
+                v[i].remaining_time -=1;
+                if(v[i].remaining_time == 0)
+                {
+                    v[i].finish_time = current_time - v[i].start_time + 1;
+                    v[i].finished = true;
+
+                }
+            }
+
+            else if(v[i].start_time <= current_time && !v[i].finished)
+            {
+                ++v[i].waiting_time;
+            }
+        }
+    }
+}
 
 void Scheduler :: schedule()
 {
@@ -61,10 +128,8 @@ void Scheduler :: schedule()
         break;
     default :
         break;
-
-
     }
 
-
+    calculateAverages();
 }
 
